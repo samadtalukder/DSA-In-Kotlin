8278c4f65ee9aa34eb8ce987d34dc0779127553d
Samad Talukder <samadtalukdar6@gmail.com>
2024-03-11 20:03:00 +0800

    ADDED: questions description

--- src/main/kotlin/leetcode/Contains_Duplicate_217.kt
+++ src/main/kotlin/leetcode/Contains_Duplicate_217.kt
@@ -1,5 +1,20 @@
 package com.samad_talukder.leetcode
 
+/*
+Given an integer array nums,
+return true if any value appears at least twice in the array,
+and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+*/
 class Contains_Duplicate_217 {
     fun containsDuplicate(nums: IntArray): Boolean {
         nums.sort()
