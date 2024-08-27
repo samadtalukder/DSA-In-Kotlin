4cc025f960d5ff1b3a4170fca4a39486cffc89bf
Samad Talukder <samadtalukdar6@gmail.com>
2024-08-27 12:24:25 +0800

    UPDATED: nullability check

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package com.samad_talukder
 
 fun main() {
+    var name: String? = null
+    showNameLength(name)
     println("Hello World!")
+}
+
+fun showNameLength(name: String?) { // Function accept nullable parameters
+    println(if (name == null) 0 else name.length)
+}
+
+fun showNameLengthElvis(name: String?) { // Function accept nullable parameters
+    println(name?.length ?: 0)
+}
+
+fun showNameLength2(name: String) { // Function accept only non-nullable parameters
+    println(name.length)
 }
