ac41d205e602fda399f5d92ba1df3e5d29cadf90
Samad Talukder <samadtalukdar6@gmail.com>
2025-03-02 15:58:11 +0600

    UPDATED: LeetCode Contains_Duplicate programs documentation

--- src/main/kotlin/leetcode/Contains_Duplicate_217.kt
+++ src/main/kotlin/leetcode/Contains_Duplicate_217.kt
@@ -1,21 +1,25 @@
 package com.samad_talukder.leetcode
 
-/*
-Given an integer array nums,
-return true if any value appears at least twice in the array,
-and return false if every element is distinct.
+/**
+ * Given an integer array nums return true if any value appears at least twice in the array, and return false if every element is distinct.
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ */
 
-Example 1:
-
-Input: nums = [1,2,3,1]
-Output: true
-
-Example 2:
-
-Input: nums = [1,2,3,4]
-Output: false
-*/
 class Contains_Duplicate_217 {
+    /**
+     * Checks if the given array contains any duplicate elements.
+     *
+     * @param nums The input array of integers.
+     * @return 'true' if duplicates exist, otherwise 'false'.
+     */
     fun containsDuplicate(nums: IntArray): Boolean {
         nums.sort()
         for (i in 1..<nums.size) {
@@ -26,7 +30,14 @@ class Contains_Duplicate_217 {
         return false
     }
 
-    fun containsDuplicateUsingSet(nums: IntArray): Boolean {
+    /**
+     * Checks if the given array contains any duplicate elements using a HashSet
+     *
+     * @param nums The input array of integers.
+     * @return 'true' if duplicates exist, otherwise 'false'.
+     */
+
+    fun containsDuplicates(nums: IntArray): Boolean {
         val setNum = mutableSetOf<Int>()
 
         for (i in nums) {
@@ -43,18 +54,8 @@ class Contains_Duplicate_217 {
 fun main() {
     val containsDuplicate217 = Contains_Duplicate_217()
 
-    val intArr = intArrayOf(1,2,3,4)
-    //println("${intArr.sort()}")
-    /*intArr.sort()
+    val intArr = intArrayOf(1,1,2,1)
 
-    for (i in 0..<intArr.size) {
-        if (intArr[i] == intArr[i - 1]) {
-            print(intArr[i])
-        } else {
-            print(intArr[i])
-        }
-
-    }*/
-    println(containsDuplicate217.containsDuplicateUsingSet(intArr))
+    println(containsDuplicate217.containsDuplicates(intArr))
 }
 
